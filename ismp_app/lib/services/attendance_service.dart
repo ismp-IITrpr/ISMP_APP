8704349433c8ee297ff8802227a0c96f6ae1790e
guptaparag101-web <guptaparag101@gmail.com>
2026-07-02 16:14:04 +0530

    Fix undefined name Color in attendance_service.dart by importing material.dart

--- ismp_app/lib/services/attendance_service.dart
+++ ismp_app/lib/services/attendance_service.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/attendance.dart';
 import '../models/events.dart';

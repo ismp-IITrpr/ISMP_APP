11470ed230e9ca52eaeb051cd99b84201b54dab0
guptaparag101-web <guptaparag101@gmail.com>
2026-07-11 15:50:18 +0530

    chore: update generated plugin registrars after pub get

--- ismp_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ismp_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,12 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import cloud_functions
 import device_info_plus
 import firebase_auth
 import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import google_sign_in_ios
 import mobile_scanner
 import share_plus
@@ -18,9 +21,12 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
